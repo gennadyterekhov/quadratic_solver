91db94a614b3ce1496ecd8ae5879ea824150f821
Gennady Terekhov <ter.ge@yandex.ru>
2021-10-26 23:13:11 +0300

    removed extra stuff

--- src/Kernel.py
+++ src/Kernel.py
@@ -13,27 +13,30 @@ class Kernel:
         self.set_action_from_parameters()
         self.perform()
 
-
     def validate_parameters(self):
         self.validate_parameters_length()
         self.validate_parameters_type()
 
     def validate_parameters_type(self):
         try:
-            self.coefficients = list(map(float, self.cli_parameters[1:self.VALID_PARAMETERS_LENGTH]))
+            self.coefficients = list(
+                map(float, self.cli_parameters[1:self.VALID_PARAMETERS_LENGTH])
+            )
         except Exception as exception:
-            raise Exception(f'Some of the parameters is not a number\n{exception}')
-    
+            raise Exception(
+                f'Some of the parameters is not a number\n{exception}'
+            )
+
     def validate_parameters_length(self):
         if len(self.cli_parameters) < self.VALID_PARAMETERS_LENGTH:
             raise Exception(
                 f'Wrong number of parameters. You must supply {self.VALID_PARAMETERS_LENGTH - 1} parameters'
             )
-    
+
     def set_action_from_parameters(self):
         if len(self.cli_parameters) > self.VALID_PARAMETERS_LENGTH:
             self.action = self.cli_parameters[self.VALID_PARAMETERS_LENGTH]
-    
+
     def perform(self):
         try:
             default_method = getattr(self, self.DEFAULT_ACTION)
@@ -41,7 +44,6 @@ class Kernel:
         except Exception as exception:
             raise Exception(f'Chosen action is not available.\n{exception}')
         return method()
-
 
     def solve(self):
         a, b, c = self.coefficients
